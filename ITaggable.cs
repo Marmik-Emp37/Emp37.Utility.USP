49565c24e8280312460ce309248abeb5cd3959ce
Marmik <144893751+Marmik-Emp37@users.noreply.github.com>
2025-06-24 18:47:40 +0530

    Initial commit
    Added ITaggable.cs for Custom Tags

---
+++ ITaggable.cs
@@ -0,0 +1,8 @@
+namespace USP
+{
+      public interface ITaggable
+      {
+            public string Tag { get; }
+            public sealed bool Compare(string tag) => Tag.Equals(tag);
+      }
+}
